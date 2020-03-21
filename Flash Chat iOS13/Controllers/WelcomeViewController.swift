628833ebe515215fade96991c20036214ebd16d6
Allan <allan.g.galdino@outlook.com>
2020-03-20 23:38:27 -0300

    first commit

---
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,49 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Allan Galdino on 03/19/2020
+//  Copyright © 2020 Allan Galdino. All rights reserved.
+//
+
+import UIKit
+import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Cocoapods will animate this text
+        titleLabel.text = K.appName
+        
+    }
+}
+
+// ====== Same effect of the pod CLTypingLabel but without Cocoapods (used in the viewDidLoad) ===================
+//
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "⚡FlashChat"
+//        for letter in titleText{
+//            Timer.scheduledTimer(withTimeInterval: (0.1 * charIndex), repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
+//
+// ===============================================================================================================
